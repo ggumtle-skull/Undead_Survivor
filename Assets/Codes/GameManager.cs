d41abcaed2e182c9d6629d157953e11e38664b8b
ggumtle_skull <huiseong8442@gmail.com>
2023-05-24 16:39:21 +0900

    2023년 5월 24일
    몬스터 스포너 생성완료 / 스폰되는 몬스터중 한 종류가 스폰되지 않음 / 기본 무기 완성 / 삽 형태를 한 무기가 플레이어 주위를 떠다니게 함

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -5,11 +5,42 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    [Header("# Game Control")]
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
+    [Header("# Player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 10, 30, 60, 100, 150, 210, 280, 360, 450, 600 };
+    [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
 
     void Awake()
     {
         instance = this;
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        exp++;
+
+        if(exp >= nextExp[level])
+        {
+            level++;
+            exp = 0;
+        }
+
     }
 }
--- Assets/Codes/Spawner.cs
+++ Assets/Codes/Spawner.cs
@@ -6,8 +6,11 @@ public class Spawner : MonoBehaviour
 {
 
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;
 
+    int level;
     float timer;
+    float timer2;
 
     private void Awake()
     {
@@ -16,17 +19,40 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        timer2 += Time.deltaTime;
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length - 1);
 
-        if (timer > 0.5f)
+        if (timer > spawnData[level].spawnTime)
         {
             Spawn();
             timer = 0;
+        }
+        if(timer2 > 0.5f)
+        {
+            mobSpawn();
+            timer2 = 0;
         }
     }
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0,4));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+    void mobSpawn()
+    {
+        GameObject enemy = GameManager.instance.pool.Get(0);
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[Random.Range(0,level)]);
+    }
+}
+
+[System.Serializable]
+public class SpawnData
+{ 
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
